1d748e569768fbc37e3e129bc222525d4b19e3bc
PIPBoy0012 <thiasbabs@gmail.com>
2020-09-24 11:07:01 +0200

    small fix

--- Forside.php
+++ Forside.php
@@ -1,8 +1,26 @@
-<!DOCTYPE html>
 <?php
+ini_set("display_errors", 0);
 
+
+function OpenCon()
+{
+  $servername = "94.177.229.154";
+  $username = "FL0D1N";
+  $password = "Pepsi1234";
+  $dbname = "Bilbixen";
+
+  // Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+  echo "dead";
+}
+echo "Connected successfully";
+}
  ?>
-
+<!DOCTYPE html>
 <html lang="en" dir="ltr">
 <head>
  <meta charset="utf-8">
@@ -22,35 +40,6 @@
 
  <h2 class="h2title">Personbiler</h2>
 
- <?php
- function OpenCon()
- {
-   $servername = "94.177.229.154";
-   $username = "FL0D1N";
-   $password = "Pepsi1234";
-   $dbname = "Bilbixen";
-
-   // Create connection
-   $conn = mysqli_connect($servername, $username, $password, $dbname);
-   // Check connection
-   if (!$conn) {
-     die("Connection failed: " . mysqli_connect_error());
-   }
-   
-  $result = mysqli_query($conn, $sql);
-
-  if (mysqli_num_rows($result) > 0) {
-    // output data of each row
-    while($row = mysqli_fetch_assoc($result)) {
-      echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
-    }
-  } else {
-    echo "0 results";
-  }
-
-  mysqli_close($conn);
-
-  ?>
 
 
 <a href="Bil.html">
@@ -81,9 +70,4 @@
 
  </footer>
  </div>
-
 </html>
-
-<?php
-
- ?>
