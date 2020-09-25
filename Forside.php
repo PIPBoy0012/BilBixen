7e84cda718dfc319d976c611587ddfa330c7c4aa
PIPBoy0012 <thiasbabs@gmail.com>
2020-09-25 08:19:48 +0200

    Code Cleanup

--- Forside.php
+++ Forside.php
@@ -26,28 +26,30 @@ echo "Connected successfully";
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
 <head>
- <meta charset="utf-8">
- <title>Bilbixen</title>
- <link rel="stylesheet" href="Forside.css">
+
+  <meta charset="utf-8">
+  <title>Bilbixen</title>
+  <link rel="stylesheet" href="Main.css">
+
 </head>
 <body>
-
  <div class="Navbar">
    <ul>
+
      <li> <a href="#">Biler</a></li>
      <li><a href="#">Om os</a></li>
      <li><a href="#">Kontakt</a></li>
      <li><a href="#">Forhandler Login</a></li>
+
    </ul>
  </div>
 
  <h2 class="h2title">Personbiler</h2>
-
-
-
 <a href="Bil.php">
+
 <div class="Imagegallery">
   <div class="Carcontainer">
+
     <img class="Carimage" src="img\371426730_c9db1a67cd_z.jpg" alt="">
     <p class="DescriptionPris">100000 Dkk</p>
     <p class="Description">Navn: allah</p>
@@ -56,21 +58,22 @@ echo "Connected successfully";
 
   </div>
 
-
   <p class="Descriptionbottom">Se alt Information</p>
+
 </div>
+
 </a>
 
-
 </body>
-
 <footer>
+
  <div class="Footerdiv">
+
    <p>Bilbixen A/S</p>
    <p>Brugtvognsvej 100</p>
    <p>3784Brugtvogn City</p>
    <p>+45 637-726-78</p>
 
- </footer>
  </div>
+ </footer>
 </html>
